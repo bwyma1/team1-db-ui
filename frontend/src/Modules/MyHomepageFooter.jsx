015b6f19264935d3a30ec758898fdb6d1f3c2496
Cyber-Aim <davidedwin@cyber-aim.com>
2022-10-26 16:45:15 -0500

    added homepage

---
+++ frontend/src/Modules/MyHomepageFooter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./MyHomepageFooter.css";
+
+//David Berberian
+export default function MyImageSlider() {
+  return (
+    <>
+     <footer>footer</footer>
+
+    </>
+  );
+}
