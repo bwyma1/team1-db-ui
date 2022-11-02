3b7d75e4027220df70d4d81f8c6eef4bf5ae3b9f
DavidBerberian <Davidedwin@montemasis.com>
2022-11-02 16:51:55 -0500

    started formatting auction page designed layouts on paper

---
+++ frontend/src/Views/Auction.jsx
@@ -0,0 +1,28 @@
+import MyImageSlider from "../Modules/MyImageSlider";
+import MyHomepageFooter from "../Modules/MyHomepageFooter.jsx";
+
+
+export default function Auction() {
+  return (
+    <div>
+    <header>Aution title</header>  
+    <img src="" alt="image"/>
+<span>
+  <div>
+  <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+  </div>
+</span>
+<ul>
+    <li>tags</li>
+    <li>tags</li>
+    <li>tags</li>
+</ul>
+
+
+<p>information</p>
+
+      <MyImageSlider/>
+      <MyHomepageFooter/>
+    </div>
+  );
+}
