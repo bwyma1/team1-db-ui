6c1fe90a811ac64353125c5c6dab8d3dd3ae3e2a
kendallboesch <83316196+kendallboesch@users.noreply.github.com>
2022-11-14 18:44:38 -0600

    push

--- frontend/src/Views/Profile.jsx
+++ frontend/src/Views/Profile.jsx
@@ -22,28 +22,6 @@ export default function Profile() {
       <div> 
         <ProfileHeader/>
       </div>
-      {/* <span id="profileContainer" class="ui fixed">
-        <img
-          class="image"
-          src="https://via.placeholder.com/150x150"
-          id="tempProfPic"
-          alt="temporary profile pic"
-        />
-
-        <section id="profileInfoContainer">
-          <h1 id="nameHeader" class="ui ">
-            {fname} {lname}
-          </h1>
-          <section id="UserAndBio">
-            <h2 id="username" class="ui">
-              @{username}
-            </h2>
-            <p id="bio" class="ui">
-              {bio}
-            </p>
-          </section>
-        </section>
-      </span> */}
       <br />
 
   
