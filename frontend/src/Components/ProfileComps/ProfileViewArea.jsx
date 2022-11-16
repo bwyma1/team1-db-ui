a68ff24a22284e70be112ed73554a611235622b6
kendallboesch <83316196+kendallboesch@users.noreply.github.com>
2022-11-16 17:17:37 -0600

    msg

--- frontend/src/Components/ProfileComps/ProfileViewArea.jsx
+++ frontend/src/Components/ProfileComps/ProfileViewArea.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react" 
-import React from "react" ;
+import { useState } from "react"
+import React from "react";
 import ItemCard from "./ItemCard";
 import ProfileViewSwitcher from "./ProfileViewSwitcher";
 import "./ProfileViewArea.css";
@@ -7,74 +7,54 @@ import ViewFilter from "./ViewFilter";
 
 
 
-export default function ProfileViewArea({items})
+export default function ProfileViewArea({ items }) 
 {
     const [auctions, setAuctions] = useState();
     useEffect(() => {
-        setAuctions(items); 
+        setAuctions(items);
         //getUserSelling(props.id).then(x => setAuctions(x)); 
-    }, []); 
-  
-    if(auctions == null) 
-    {
-        return <div>loading...</div>; 
+    }, []);
+
+    if (auctions == null) {
+        return <div>loading...</div>;
     }
- 
-   
 
-    return(
+
+
+    return (
         <>
-        <ProfileViewSwitcher />
-        <div class="view2Area">
-            
-            {/* <div> 
-               // <ViewFilter/>
-            </div> */}
-            <div class='viewArea'> 
+            <ProfileViewSwitcher />
+            <div class="view2Area">
 
 
-             <ItemCard/>
-             <ItemCard/>
-             <ItemCard/>
-             <ItemCard/>
-             <ItemCard/>
-             <ItemCard/>
-             <ItemCard/>
-             <ItemCard/>
-             <ItemCard/>
-             <ItemCard/>
-             <ItemCard/>
-             <ItemCard/>
-             <ItemCard/>
+
             </div>
+            <>
 
-        </div>
-        <>
-    
-                    {/* <ReviewForm onReviewAdded = { review => addReview({reviews: [...product.reviews, review]})}/> */}
+                {/* <ReviewForm onReviewAdded = { review => addReview({reviews: [...product.reviews, review]})}/> */}
 
-        <div class="view2Area" className=' container list-unstyled d-flex justify-content-center flex-wrap w-100'>
-                {auctions.map((auction, index) => (
-                    <li className ='m-2' key ={index}> 
-                    <div className='card h-100 m-3 mb-3'> 
-                        <ItemCard
-                                 Image = {auction.Image}
-                                 Title ={auction.Title}
-                                 OwnerName = {auction.OwnerName}
-                                 StartPrice = {auction.StartPrice}
-                                 LeadBid={auction.LeadBid}
-                                 EndDate={auction.EndDate}
-                                 Description={auction.Description}/>    
-                                 
-                    </div>
-                 
-                    </li> 
-                ))}
+                <div class="view2Area" className=' container list-unstyled d-flex justify-content-center flex-wrap w-100'>
+                    {auctions.map((auction, index) => (
+                        <li className='m-2' key={index}>
+                            <div className='card h-100 m-3 mb-3'>
+                                <ItemCard
+                                    Image={auction.Image}
+                                    Title={auction.Title}
+                                    OwnerName={auction.OwnerName}
+                                    StartPrice={auction.StartPrice}
+                                    LeadBid={auction.LeadBid}
+                                    EndDate={auction.EndDate}
+                                    Description={auction.Description} />
 
-               </div>
+                            </div>
 
-        </>
+                        </li>
+                    ))}
 
-    );
+                </div>
+                </>
+
+            </>);
+
+
 }
-
