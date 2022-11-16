ff87b1d315b0a8a5f5830d086b2fb5f4cfc5e002
milesfried <mafriedman@smu.edu>
2022-11-16 16:41:07 -0600

    user route added

--- backend/routes.js
+++ backend/routes.js
@@ -7,4 +7,5 @@ module.exports = function(app, logger)
 // USER 
 app.route("/login/").post(userController.login_user);
 app.route("/users/").get(userController.get_users);
+app.route("/user/:email").get(userController.get_user);
 };
