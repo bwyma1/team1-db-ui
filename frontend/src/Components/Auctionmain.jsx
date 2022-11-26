f979d753a9d74fc1f5a4862e8e5d745455f74daa
DavidBerberian <Davidedwin@montemasis.com>
2022-11-26 15:52:34 -0600

    Css and setup for linking this page to backend
    I redid the css to look better and redid the logic to work with the separate comments object and auctions. also set up api by id to use.

--- frontend/src/Components/Auctionmain.jsx
+++ frontend/src/Components/Auctionmain.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import "./Auctionmain.css"
 import "./MyHomepageFooter.css";
 import { auction } from "../Models"
-import { Badge } from "@mantine/core";
+import { Badge, Card } from "@mantine/core";
 import { useParams } from "react-router-dom"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
 //import getUserById from "../api";
 
 let selected_auction = new auction(
   1,
   "Brock",
   "The art of War",
-  "This piece depicts the wonderful art of war",
+  "This piece depicts the wonderful art of war ",
   "129.99",
   "https://via.placeholder.com/150x150",
   [
@@ -26,11 +27,21 @@ let selected_auction = new auction(
 //David Berberian
 export default function Auctionmain() {
 
+
+
   
-const params = useParams()
 
+  // <ReviewForm onReviewAdded={review => {
+  //   addReview(params.Id, review);
+  //   addReviewToList({ reviews: [...product.reviews, review]})}}/>
+//const params = useParams()
 
-useEffect(() =>{
+const[Auction, setAuction] = useState('');
+const[comments, setCommments] = useState([]);
+
+useEffect(() =>{//selected auction to auction
+//call get auction by id
+//call get comment by id
 
 
 
@@ -38,56 +49,80 @@ useEffect(() =>{
 })
   
   return (
-    <>
-
-
-
-    <h2 id="piecename">{selected_auction.title}</h2>  
-
-    <div>
-    <img id="picture1" src={selected_auction.imageUrl} alt="image"/>
-    <div id="tag1">{selected_auction.tags.map((tag) => (
+    <div className="">
+      
+      
+ <div><h2 id="piecename">{selected_auction.title}</h2></div>
+ <span id="sellerbox"><Badge color="cyan" variant="light">Seller: {selected_auction.seller}</Badge></span>
+ <span id="tag1">{selected_auction.tags.map((tag) => (
                     <Badge color="pink" variant="light">
                       {tag}
                     </Badge>
-                  ))} </div>
-  <p id="description">{selected_auction.description}</p>
-  </div>
+                  ))} </span>
+
+
+<span id="forms">
+<form id="biddingform">
+<h3>Price: {selected_auction.price}</h3>
+<label id="label1" for="bid">Bid</label>
+<input type="text" id="bid"/>
+<button type="submit" id="bidbtn">Bid</button>
+</form>
+ <span id="description"><h5>Description:</h5>< >{selected_auction.description}</></span>
+</span>
+
+
+<div>
+    <img id="picture1" src={selected_auction.imageUrl} alt="image"/>
+
+ 
+</div>
+  
+  
+    
+
+   
 <div id="bottomcontent">
 
 </div>
+<br/>
 <div id="timer">
-<h3 >Timer</h3>
+
 </div>
 
 
-<form id="biddingform">
 
-<label for="bid">Bid:</label>
-<input type="text" id="bid"/>
-<button type="submit" id="bidbtn">Bid</button>
-
-</form>
-
-<form id="commentarybox">
-<label for="commentbox">Comment</label>
-<textarea id="commentbox"></textarea>
-
-</form>
-
-
+<br/>
 <h4>Comments</h4>
-<ul id="tag2">
+<div id="tag2">
 {selected_auction.comments.map((comment) => (
-                    <li>
+                    <Card>
+                      <div className="user" style={{ fontWeight: 'bold' }}>Name:  {comment[0]}</div>
+    <div className="comment" style={{ marginBottom: '20px' }}>{comment[1]}</div>
+   
                       
-                      {comment[0]}
-                    </li>
+                    </Card>
                   ))}
-</ul>
+</div>
+
+<Card>
+<form id="commentarybox">
+<label id="label2" for="commentbox">Comment</label>
+<textarea id="commentbox"></textarea>
+<br/>
+<div id="btnline">
+  <button type="submit" id="commentbtn" onClick={() => {
+
+
+  }}>Submit</button>
+</div>
+
+</form>
+</Card>
+
      <footer>footer</footer>
 
-    </>
+    </div>
   );
 }
 
