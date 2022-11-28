ff9fac432ff95647c2c35e6e2d52d1f150f4e0a4
diegparedes <91636196+diegparedes@users.noreply.github.com>
2022-11-27 21:25:40 -0600

    fixed errors with route exports

--- backend/model/Notifications.js
+++ backend/model/Notifications.js
@@ -9,7 +9,7 @@ var Notification = function(user)
 }
 
 // app.route("/notifications/").get(notificationController.get_notifications);
-Notification.get_notifications = function(result)
+exports.get_notifications = function(result)
 {
     sql.query("SELECT * FROM Notifications", function(err, res)
     {
@@ -27,7 +27,7 @@ Notification.get_notifications = function(result)
 };
 
 // app.route("/notifications/:NotificationID").get(notificationController.get_notification_id);
-Notification.get_notification_id = function(NotificationID, result)
+exports.get_notification_id = function(NotificationID, result)
 {
     sql.query("SELECT * FROM Notifications WHERE NotificationID = ?", NotificationID, function(err, res)
     {
@@ -44,7 +44,7 @@ Notification.get_notification_id = function(NotificationID, result)
 };
 
 // app.route("/notifications/:NotificationID").put(notificationController.update_notification);
-Notification.update_notification = function(NotificationID, Notification, result)
+exports.update_notification = function(NotificationID, Notification, result)
 {
     sql.query("UPDATE Notifications SET UserEmail = ?, NotificationMessage = ? WHERE NotificationID = ?", [Notification.UserEmail, Notification.NotificationMessage, NotificationID], function(err, res)
     {
@@ -61,7 +61,7 @@ Notification.update_notification = function(NotificationID, Notification, result
 };
 
 // app.route("/notifications/").post(notificationController.create_notification);
-Notification.create_notification = function(Notification, result)
+exports.create_notification = function(Notification, result)
 {
     sql.query("INSERT INTO Notifications SET ?", Notification, function(err, res)
     {
@@ -78,7 +78,7 @@ Notification.create_notification = function(Notification, result)
 };
 
 // app.route("/notifications/:NotificationID").delete(notificationController.delete_notification);
-Notification.delete_notification = function(NotificationID, result)
+exports.delete_notification = function(NotificationID, result)
 {
     sql.query("DELETE FROM Notifications WHERE NotificationID = ?", NotificationID, function(err, res)
     {
@@ -95,7 +95,7 @@ Notification.delete_notification = function(NotificationID, result)
 };
 
 // app.route("/notifications/:Email").get(notificationController.get_notifications);
-Notification.get_notifications = function(Email, result)
+exports.get_notifications = function(Email, result)
 {
     sql.query("SELECT * FROM Notifications WHERE UserEmail = ?", Email, function(err, res)
     {
