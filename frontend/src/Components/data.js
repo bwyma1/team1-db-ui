5bf8f5434334c8efe4a9150347aab5296fcd0ba3
kendallboesch <83316196+kendallboesch@users.noreply.github.com>
2022-11-29 12:18:19 -0600

    yay

---
+++ frontend/src/Components/data.js
@@ -0,0 +1,30 @@
+// import { auction } from "../Models"
+// import { user } from "../Models"
+// export default [
+
+
+//     {
+//         new user( "kendall.boeschh",
+//                  "Kendall Boesch",
+//                  "Selling art!!",
+//                  "https://via.placeholder.com/150x150",
+//                  [],
+//              [new auction(0, "@seller2", "title like00", "desc", 8.00, "https://via.placeholder.com/150x150", [], []),
+//         new auction(1, "@seller2", "title like01", "desc", 8.00, "https://via.placeholder.com/150x150", [], []),
+//         new auction(2, "@seller3", "title like02", "desc", 8.00, "https://via.placeholder.com/150x150", [], []),
+//         new auction(13, "@johnnyd123", "title s5", "desc", 8.00, "https://via.placeholder.com/150x150", [], [])],
+//          [
+//             new auction(3, '@kendall.boeschh', "title sel1 ", "desc", 8.00, "https://via.placeholder.com/150x150", [], []),
+//             new auction(4, "@kendall.boeschh", "title sel2", "desc", 8.00, "https://via.placeholder.com/150x150", [], []),
+//             new auction(5, "@kendall.boeschh", "title sel3", "desc", 8.00, "https://via.placeholder.com/150x150", [], [])],
+//          [
+//             new auction(6, '@seller', "title pur1 ", "desc", 8.00, "https://via.placeholder.com/150x150", [], []),
+//             new auction(7, "@sellwe", "title pur2", "desc", 8.00, "https://via.placeholder.com/150x150", [], []),
+//             new auction(8, "@seller", "title pur3", "desc", 8.00, "https://via.placeholder.com/150x150", [], [])],
+//          [
+//             new auction(9, '@seller', "title act1 ", "desc", 8.00, "https://via.placeholder.com/150x150", [], []),
+//             new auction(10, "@sellwe", "title act2", "desc", 8.00, "https://via.placeholder.com/150x150", [], []),
+//             new auction(11, "@seller", "title act3", "desc", 8.00, "https://via.placeholder.com/150x150", [], []) ])
+
+// }; 
+
