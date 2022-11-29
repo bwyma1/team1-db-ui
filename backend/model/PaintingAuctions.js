1c1c525b4af8531361479b23003311baad20e938
milesfried <mafriedman@smu.edu>
2022-11-28 21:46:27 -0600

    changed tags

--- backend/model/PaintingAuctions.js
+++ backend/model/PaintingAuctions.js
@@ -11,7 +11,11 @@ var PaintingAuction = function(user)
   this.Image = PaintingAuction.Image;
   this.StartPrice = PaintingAuction.StartPrice;
   this.DateListed = PaintingAuction.DateListed;
-  this.Tags = PaintingAuction.Tags;
+  this.Pencil = PaintingAuction.Pencil;
+  this.Paint = PaintingAuction.Paint;
+  this.Modern = PaintingAuction.Modern;
+  this.Abstract = PaintingAuction.Abstract;
+  this.Realism = PaintingAuction.Realism;
   this.EndDate = PaintingAuction.EndDate;
 }
 exports.get_auction_id = function(req, res)
@@ -192,14 +196,18 @@ exports.create_auction = function(req, res)
   {
     sql.connection.query
     (
-      "INSERT INTO `PaintingAuctions` (`OwnerName`, `LeadBid`, `Image`, `StartPrice`, `DateListed`, `Tags`, `EndDate`) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
+      "INSERT INTO `PaintingAuctions` (`OwnerName`, `LeadBid`, `Image`, `StartPrice`, `DateListed`, `Paint`, `Pencil`, `Modern`, `Abstract`, `Realism`, `EndDate`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
       [
         req.body.OwnerName,
         req.body.LeadBid,
         req.body.Image,
         req.body.StartPrice,
         req.body.DateListed,
-        req.body.Tags,
+        req.body.Paint,
+        req.body.Pencil,
+        req.body.Modern,
+        req.body.Abstract,
+        req.body.Realism,
         req.body.EndDate,
       ],
       function(sqlErr, sqlRes)
