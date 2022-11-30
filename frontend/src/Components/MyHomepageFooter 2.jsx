39076a4522249d3f8a2f472f2369a5aaa8cacebb
kendallboesch <83316196+kendallboesch@users.noreply.github.com>
2022-11-30 03:48:27 -0600

    adding files

---
+++ frontend/src/Components/MyHomepageFooter 2.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./MyHomepageFooter.css";
+
+//David Berberian
+export default function MyHomepageFooter() {
+
+  return (
+    <>
+     <footer></footer>
+
+    </>
+  );
+}
