812e98823619fc297fc7ecea9b27c43010477427
milesfried <mafriedman@smu.edu>
2022-11-29 22:46:29 -0600

    Update user.js

--- backend/model/user.js
+++ backend/model/user.js
@@ -82,14 +82,15 @@ exports.create_user = function(req, res)
   {
     var newUser = new User(req.body);
     sql.connection.query(
-      "INSERT INTO `Users` (`Email`, `DisplayName`, `Bio`, `ProfilePic`, `Tags`, `Password`) VALUES (?, ?, ?, ?, ?, ?);",
+      "INSERT INTO `Users` (`Email`, `DisplayName`, `Bio`, `ProfilePic`, `Tags`, `Password`, `Strikes`) VALUES (?, ?, ?, ?, ?, ?, ?);",
       [
         req.body.Email, 
         req.body.DisplayName, 
         req.body.Bio, 
         req.body.ProfilePic, 
         req.body.Tags, 
-        req.body.Password
+        req.body.Password,
+        req.body.Strikes
       ],
       function(sqlErr, sqlRes)
       {
